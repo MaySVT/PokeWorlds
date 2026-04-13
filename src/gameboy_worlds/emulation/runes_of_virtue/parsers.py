afeng-29/GameBoyWorlds

Add Ultima: Runes of Virtue to PokéWorlds framework
- Add runes_of_virtue emulation module with minimal parser and registry
- Register game in global emulation registry
- Add runes_of_virtue_rom_data_path config entry
- Enable debug_mode for development

Co-Authored-By: Claude Sonnet 4.6 <noreply@anthropic.com>

src/poke_worlds/emulation/runes_of_virtue/parsers.py
@@ -1,0 +1,23 @@
+from poke_worlds.utils import verify_parameters, log_error
+from poke_worlds.emulation.parser import StateParser
+
+
+class RunesOfVirtueParser(StateParser):
+    """
+    Minimal parser scaffold for Ultima: Runes of Virtue.
+    """
+
+    VARIANT = "runes_of_virtue"
+
+    def __init__(self, pyboy, parameters):
+        verify_parameters(parameters)
+        if f"{self.VARIANT}_rom_data_path" not in parameters:
+            log_error(
+                f"ROM data path not found for variant: {self.VARIANT}. Add {self.VARIANT}_rom_data_path to config files.",
+                parameters,
+            )
+        self.rom_data_path = parameters[f"{self.VARIANT}_rom_data_path"]
+        super().__init__(pyboy, parameters)
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(variant={self.VARIANT})"

MaySVT/PokeWorlds

src/gameboy_worlds/emulation/runes_of_virtue/parsers.py
@@ -1,0 +1,23 @@
+from gameboy_worlds.utils import verify_parameters, log_error
+from gameboy_worlds.emulation.parser import StateParser
+
+
+class RunesOfVirtueParser(StateParser):
+    """
+    Minimal parser scaffold for Ultima: Runes of Virtue.
+    """
+
+    VARIANT = "runes_of_virtue"
+
+    def __init__(self, pyboy, parameters):
+        verify_parameters(parameters)
+        if f"{self.VARIANT}_rom_data_path" not in parameters:
+            log_error(
+                f"ROM data path not found for variant: {self.VARIANT}. Add {self.VARIANT}_rom_data_path to config files.",
+                parameters,
+            )
+        self.rom_data_path = parameters[f"{self.VARIANT}_rom_data_path"]
+        super().__init__(pyboy, parameters)
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(variant={self.VARIANT})"